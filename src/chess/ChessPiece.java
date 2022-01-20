162453c9e69f1f2b5f4887656308ad7fff29a05b
cahzerohenrique@gmail.com <CarlosDevBr>
2022-01-19 22:14:03 -0300

    Bishop included

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -22,6 +22,10 @@ public abstract class ChessPiece extends Piece {
 		return color;
 	}
 	
+	public int getMoveCount() {
+		return moveCount;
+	}
+	
 	public void increaseMoveCount() {
 		moveCount++;
 	}
