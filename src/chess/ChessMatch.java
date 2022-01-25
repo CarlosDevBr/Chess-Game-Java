4306279417060d4f8beec3c968bde8c83b4c6165
cahzerohenrique@gmail.com <CarlosDevBr>
2022-01-25 19:16:15 -0300

    added Queen(and Pawn bugfix)

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -11,6 +11,7 @@ import chess.pieces.Bishop;
 import chess.pieces.King;
 import chess.pieces.Knight;
 import chess.pieces.Pawn;
+import chess.pieces.Queen;
 import chess.pieces.Rook;
 
 public class ChessMatch {
@@ -194,6 +195,7 @@ public class ChessMatch {
 	}
 
 	private void initialSetup() {
+		placeNewPiece('d', 1, new Queen(board, Color.WHITE));
 		placeNewPiece('g', 1, new Knight(board, Color.WHITE));
 		placeNewPiece('b', 1, new Knight(board, Color.WHITE));
 		placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
@@ -210,6 +212,7 @@ public class ChessMatch {
         placeNewPiece('g', 2, new Pawn(board, Color.WHITE));
         placeNewPiece('h', 2, new Pawn(board, Color.WHITE));
 
+        placeNewPiece('d', 8, new Queen(board, Color.BLACK));
         placeNewPiece('g', 8, new Knight(board, Color.BLACK));
         placeNewPiece('b', 8, new Knight(board, Color.BLACK));
         placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
