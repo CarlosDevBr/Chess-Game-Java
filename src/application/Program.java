26c1be3b3e72486aea18e881824dbe353388446b
cahzerohenrique@gmail.com <CarlosDevBr>
2022-01-25 21:51:35 -0300

    Special move - promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -37,6 +37,11 @@ public class Program {
 				if (capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
+				if(chessMatch.getPromoted() != null) {
+					System.out.println("Enter piece for promotion (B/N/R/Q): ");
+					String type = sc.nextLine();
+					chessMatch.replacePromotedpiece(type);
+				}
 			}
 			catch(ChessException e) {
 				System.out.println(e.getMessage());
